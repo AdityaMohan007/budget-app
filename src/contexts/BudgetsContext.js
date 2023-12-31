610b552a719ce52cce9047127a9b5f1fd9719949
AdityaMohan007 <adityamohan0607@gmail.com>
2023-12-31 21:28:59 +0530

    useLocalStorage hook

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react"
 import { v4 as uuidV4 } from 'uuid';
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const BudgetsContext = React.createContext();
 
@@ -9,8 +10,8 @@ export function useBudgets() {
 
 export const BudgetsProvider = ({ children }) => {
 
-    const [budgets, setBudgets] = useState([]);
-    const [expenses, setExpenses] = useState([]);
+    const [budgets, setBudgets] = useLocalStorage("budgets", []);
+    const [expenses, setExpenses] = useLocalStorage("expenses", []);
 
     // If we want to get all expenses for entertainment, we just pass in entertainment budget ID and we get entertainment expenses
     function getBudgetExpenses(budgetId) {
